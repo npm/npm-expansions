7554befbdf8e22e720d0e0bd99b0ccd0271a077f
Micah Williamson <micahwllmsn@gmail.com>
2015-01-02 07:40:36 -0600

    using JSON.parse for more verbose errors. bin writes where it reads. fixed JSON syntax error (did no one test this?). added "Never Push to Master" to index

--- bin/alphabetize.js
+++ bin/alphabetize.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 var fs = require("fs")
-var list = require("../index.json")
+var list = JSON.parse(fs.readFileSync("../index.json"));
 
 list.sort(function (a, b) {
   return a.toLowerCase().localeCompare(b.toLowerCase())
 })
 
-fs.writeFileSync("./index.json", JSON.stringify(list, null, 2) + "\n")
+fs.writeFileSync("../index.json", JSON.stringify(list, null, 2) + "\n")
