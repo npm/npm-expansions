5227fc039867a1ce0aab5521db8cc1445d65d722
nopeless <38830903+nopeless@users.noreply.github.com>
2022-05-23 02:04:28 +0900

    format build file

--- build.js
+++ build.js
@@ -1,32 +1,28 @@
-var fs       = require("fs")
-var infile   = __dirname + '/expansions.txt'
-var outfile_pre  = __dirname + '/expansions'
-var outfile_json = outfile_pre + '.json'
-var outfile_txt = outfile_pre + '.txt'
+/* eslint-disable camelcase */
+const fs = require('fs');
 
-var list = fs
+const infile = `${__dirname}/expansions.txt`;
+const outfile_pre = `${__dirname}/expansions`;
+const outfile_json = `${outfile_pre}.json`;
+const outfile_txt = `${outfile_pre}.txt`;
+
+const list = fs
   .readFileSync(infile, 'utf8')
-  .split("\n")
-  .map(function(e) { return e.trim() })
-  .filter(function(e) { return (e.length > 0) })
-  .filter(function(e) { return e.charAt(0) !== '#' })
-  .sort(function (a, b) {
-    return a.toLowerCase().localeCompare(b.toLowerCase());
-  })
+  .split('\n')
+  .map((e) => e.trim())
+  .filter((e) => (e.length > 0))
+  .filter((e) => e.charAt(0) !== '#')
+  .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 
-fs.writeFileSync(outfile_json, JSON.stringify(list, null, 2))
+fs.writeFileSync(outfile_json, JSON.stringify(list, null, 2));
 
-fs.writeFileSync(outfile_txt, list.reduce(
-  function(p, c){
-    return p + c + '\n'
-  }, list.shift() + '\n')
-)
+fs.writeFileSync(outfile_txt, list.reduce((p, c) => `${p + c}\n`, `${list.shift()}\n`));
 // reappend the instructions
 fs.appendFileSync(
   outfile_txt,
-  "#\n" +
-  "# please don't add your expansions down here!\n" +
-  "# insert them in alphabetical order to help reduce merge conflicts.\n" +
-  "#\n" +
-  "# <3\n"
-)
+  '#\n'
+  + "# please don't add your expansions down here!\n"
+  + '# insert them in alphabetical order to help reduce merge conflicts.\n'
+  + '#\n'
+  + '# <3\n',
+);
